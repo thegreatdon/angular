d5c21227acedd35a79c676d432e3dd43cef3caaf
thegreatdon <jose_espanol@hotmail.com>
2016-11-17 13:57:25 -0500

    changed to nprstations

--- app/app.component.js
+++ app/app.component.js
@@ -15,18 +15,24 @@ var Station = (function () {
     return Station;
 }());
 exports.Station = Station;
+var STATIONS = [
+    { id: 11, name: 'WAMU-FM' },
+    { id: 12, name: 'WBUR-FM' },
+    { id: 13, name: 'KACU-FM' }
+];
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'NPR Stations';
-        this.call_letter = {
-            id: 1,
-            name: 'WAMU-FM'
-        };
+        this.title = 'NPRStations';
+        this.stations = STATIONS;
     }
+    AppComponent.prototype.onSelect = function (station) {
+        this.selectedStation = station;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>{{call_letter.name}} details!</h2>\n    <div><label>id: </label>{{call_letter.id}}</div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"call_letter.name\" placeholder=\"name\">\n    </div>\n    "
+            template: "\n    <h1>{{title}}</h1>\n    <h2>NPRStations</h2>\n    <ul class=\"stations\">\n      <li *ngFor=\"let station of stations\"\n        [class.selected]=\"station === selectedStation\"\n        (click)=\"onSelect(station)\">\n        <span class=\"badge\">{{station.id}}</span> {{station.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedStation\">\n      <h2>{{selectedStation.name}} details!</h2>\n      <div><label>id: </label>{{selectedStation.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedStation.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n  ",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .stations {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .stations li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .stations li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .stations li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .stations .text {\n      position: relative;\n      top: -3px;\n    }\n    .stations .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,22 +3,88 @@ export class Station {
   id: number;
   name: string;
 }
+const STATIONS: Station[] = [
+  { id: 11, name: 'WAMU-FM' },
+  { id: 12, name: 'WBUR-FM' },
+  { id: 13, name: 'KACU-FM' }
+
+];
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{call_letter.name}} details!</h2>
-    <div><label>id: </label>{{call_letter.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="call_letter.name" placeholder="name">
+    <h2>NPRStations</h2>
+    <ul class="stations">
+      <li *ngFor="let station of stations"
+        [class.selected]="station === selectedStation"
+        (click)="onSelect(station)">
+        <span class="badge">{{station.id}}</span> {{station.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedStation">
+      <h2>{{selectedStation.name}} details!</h2>
+      <div><label>id: </label>{{selectedStation.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedStation.name" placeholder="name"/>
+      </div>
     </div>
-    `
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .stations {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .stations li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .stations li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .stations li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .stations .text {
+      position: relative;
+      top: -3px;
+    }
+    .stations .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'NPR Stations';
-  call_letter : Station = {
-    id: 1,
-    name: 'WAMU-FM'
-  };
+  title = 'NPRStations';
+  stations = STATIONS;
+  selectedStation: Station;
+  onSelect(station: Station): void {
+    this.selectedStation = station;
+  }
 }
